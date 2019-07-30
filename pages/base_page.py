a26c52414fe225d1180a3fa19c9e5526bc0cc00e
Henryona <henryona@gmail.com>
2019-07-30 12:28:56 +0300

    функция solve_quiz_and_get_code перенесена в base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 class BasePage(object):
     def __init__(self, browser, url, timeout=5):
@@ -9,6 +11,19 @@ class BasePage(object):
 	
     def open(self):
         self.browser.get(self.url)
+		
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            print("Your code: {}".format(alert.text))
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
 
     def is_element_present(self, how, what):
         try:
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,7 +1,5 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-from selenium.common.exceptions import NoAlertPresentException
-import math
 
 
 class ProductPage(BasePage):
@@ -15,19 +13,6 @@ class ProductPage(BasePage):
         # находим кнопку и добавляем товар в корзину
         addButton = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
         addButton.click()
-
-    def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            print("Your code: {}".format(alert.text))
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
     
     def right_book_and_right_price_message(self, bookToCompare, priceToCompare):
         self.should_be_right_book_name(bookToCompare)
